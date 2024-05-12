98be3cd8ec7ffb0f33113a3155b99324d38b1038
TheBossMan110 <syedzakihaider2006@gmail.com>
2024-05-12 13:49:35 +0500

    Project

---
+++ compare/app.js
@@ -0,0 +1,17 @@
+var data = [{
+    "id" : 0,
+    "title": "",
+    "Price": "",
+    "key Features":"",
+    "image":"",
+},{
+    "id" : 1,
+    "title": "Dawlance Air Conditioner Frost Inverter 30 DC Inverter 1.5 Ton Split AC Cool Only 14000 BTU",
+    "Price": "Rs. 170,500.",
+    "key Features":"12 Years Compressor Warranty, 4 Years Card Warranty, Auto Restart, Hidden LED Display, Timer, Indoor Unit Cooling Operating Range (° C) 17 ~ 32,Gold Fin, EER = 3.01, Air Flow Rate m3/hr = 835, Sleep mode, Removable/ Washable Panel, Self Diagnosis, Turbo Mode,  Memory Function, Anti-Rust Outdoor Casing,   Moisture removal L/h:1.5,   Voltage Range 150 - 240",
+    "image":"",
+}
+
+
+
+]
